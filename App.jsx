af50cf7483fdc9e005cd07edd16e939a88799dfb
[nemuoto] <82487969+nemuoto@users.noreply.github.com>
2021-12-06 13:35:01 +0900

    hello!

--- App.jsx
+++ App.jsx
@@ -12,10 +12,10 @@ export default function App() {
       </View>
 
       <View>
-        <View>
+        <View style={styles.memoListItem}>
           <View>
-            <Text>買い物リスト</Text>
-            <Text>2021年12月05日 10:00</Text>
+            <Text style={styles.memoListTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2021年12月05日 10:00</Text>
           </View>
           <View>
             <Text>X</Text>
@@ -24,10 +24,10 @@ export default function App() {
       </View>
 
       <View>
-        <View>
+        <View style={styles.memoListItem}>
           <View>
-            <Text>買い物リスト</Text>
-            <Text>2021年12月05日 10:00</Text>
+            <Text style={styles.memoListTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2021年12月05日 10:00</Text>
           </View>
           <View>
             <Text>X</Text>
@@ -36,10 +36,10 @@ export default function App() {
       </View>
 
       <View>
-        <View>
+        <View style={styles.memoListItem}>
           <View>
-            <Text>買い物リスト</Text>
-            <Text>2021年12月05日 10:00</Text>
+            <Text style={styles.memoListTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2021年12月05日 10:00</Text>
           </View>
           <View>
             <Text>X</Text>
@@ -47,8 +47,10 @@ export default function App() {
         </View>
       </View>
 
-      <View>
-        <Text>+</Text>
+
+
+      <View style={styles.circleButton}>
+        <Text style={styles.circleButtonLabel}>+</Text>
       </View>
     </View>
   );
@@ -81,5 +83,45 @@ const styles = StyleSheet.create({
     color: '#ffffff',
     fontWeight: 'bold',
   },
+
+  memoListItem: {
+    backgroundColor: '#ffffff',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 16,
+    paddingHorizontal: 19,
+    borderBottomWidth: 1,
+    borderColor: 'rgba(0,0,0,0.15)',
+  },
+  memoListTitle: {
+    fontSize: 16,
+    lineHeight: 32,
+  },
+  memoListItemDate: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: '#848484',
+  },
+  circleButton: {
+    backgroundColor: '#467fd3',
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position:'absolute',
+    right:40,
+    bottom:40,
+    shadowColor:'#000000',
+    shadowOffset:{width:0,height:8},
+    shadowOpacity:0.25,
+    shadowRadius:8,
+    elevation:8,
+  },
+  circleButtonLabel: {
+    color: '#ffffff',
+    fontSize:40,
+    lineHeight:40,
+  }
 
 });
